979f5d264e1954b4c0d04968a8a9da4784feb7a3
ofekyem <ofekyemini87@gmail.com>
2024-02-13 12:33:46 +0200

    sign up a new user is add to the list

--- app/src/main/java/com/example/androidfacebook/pid/Pid.java
+++ app/src/main/java/com/example/androidfacebook/pid/Pid.java
@@ -20,6 +20,12 @@ public class Pid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pid);
+        User user = (User)getIntent().getSerializableExtra("USER");
+        //the user that is in the pid now
+
+
+
+
 
         RecyclerView lstPosts = findViewById(R.id.lstPosts);
         final PostsListAdapter adapter = new PostsListAdapter(this);
@@ -28,10 +34,12 @@ public class Pid extends AppCompatActivity {
         User a = new User("foobar","foo123","foo bar",R.drawable.ic_facebook);
         Post p1 = new Post(1,a,"Hello There","12.2.2024",R.drawable.picture1);
         Post p2 = new Post(2,a,"Hey Facebook","13.2.2024",R.drawable.picture1);
+        Post p3 = new Post(2,user,"Hey Facebook","13.2.2024",R.drawable.picture1);
         List <Post> posts;
         posts = new ArrayList<>();
         posts.add(p1);
         posts.add(p2);
+        posts.add(p3);
         adapter.setPosts(posts);
 
     }
