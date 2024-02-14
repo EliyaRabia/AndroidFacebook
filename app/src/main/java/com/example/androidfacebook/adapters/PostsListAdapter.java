33ffdf43d34ecd62d0e4b59296472c07c71d86a3
EliyaRabia <eliyarabia@gmail.com>
2024-02-14 14:08:36 +0200

    add post

--- app/src/main/java/com/example/androidfacebook/adapters/PostsListAdapter.java
+++ app/src/main/java/com/example/androidfacebook/adapters/PostsListAdapter.java
@@ -2,7 +2,6 @@ package com.example.androidfacebook.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -22,6 +21,7 @@ public class PostsListAdapter extends RecyclerView.Adapter<PostsListAdapter.Post
         private final TextView tvContent;
         private final ImageView ivPic;
         private final ImageButton btnLike;
+        private final ImageButton btnShare;
 
         private PostViewHolder(View itemView){
             super(itemView);
@@ -29,30 +29,12 @@ public class PostsListAdapter extends RecyclerView.Adapter<PostsListAdapter.Post
             tvContent=itemView.findViewById(R.id.tvContent);
             ivPic=itemView.findViewById(R.id.ivPic);
             btnLike = itemView.findViewById(R.id.likeButton);
+            btnShare = itemView.findViewById(R.id.shareButton);
         }
         private void showPopupMenu(View view) {
             PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
             popupMenu.getMenuInflater().inflate(R.menu.share_menu, popupMenu.getMenu());
 
-            // Set a listener for menu item clicks
-            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem menuItem) {
-                    // Handle menu item clicks here
-                    switch (menuItem.getItemId()) {
-                        case R.id.action_share_facebook:
-                            // Handle Facebook share action
-                            break;
-                        case R.id.action_share_twitter:
-                            // Handle Twitter share action
-                            break;
-                        case R.id.action_share_email:
-                            // Handle Email share action
-                            break;
-                    }
-                    return true;
-                }
-            });
 
             // Show the popup menu
             popupMenu.show();
@@ -77,20 +59,27 @@ public class PostsListAdapter extends RecyclerView.Adapter<PostsListAdapter.Post
 
             holder.btnLike.setOnClickListener(v -> {
                 if (current.isLiked()) {
-                // Change the icon to icon1
+                    // Change the icon to icon1
                     holder.btnLike.setImageResource(R.drawable.like_svgrepo_com);
-                // Set liked to false
-                current.setLiked(false);
+                    // Set liked to false
+                    current.setLiked(false);
 //                 Decrease the number of likes here
 //                post.setLikes(post.getLikes() - 1);
-            } else {
-                // Change the icon to icon2
+                } else {
+                    // Change the icon to icon2
                     holder.btnLike.setImageResource(R.drawable.like_icon);
-                // Set liked to true
-                current.setLiked(true);
+                    // Set liked to true
+                    current.setLiked(true);
 //                 Increase the number of likes
 //                post.setLikes(post.getLikes() + 1);
-            }
+                }
+            });
+            holder.btnShare.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Show the popup menu when the share button is clicked
+                    holder.showPopupMenu(view);
+                }
             });
         }
     }
