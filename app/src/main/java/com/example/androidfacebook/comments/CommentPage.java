32c58122f8da20658099935ac03aea3249ebe4fa
ofekyem <ofekyemini87@gmail.com>
2024-02-15 20:32:13 +0200

    adding comment and edit comment works

--- app/src/main/java/com/example/androidfacebook/comments/CommentPage.java
+++ app/src/main/java/com/example/androidfacebook/comments/CommentPage.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,6 +17,7 @@ import com.example.androidfacebook.entities.ClientUser;
 import com.example.androidfacebook.entities.Comment;
 import com.example.androidfacebook.entities.DataHolder;
 import com.example.androidfacebook.entities.Post;
+import com.example.androidfacebook.login.Login;
 import com.example.androidfacebook.pid.Pid;
 
 import java.util.List;
@@ -37,11 +40,33 @@ public class CommentPage extends AppCompatActivity {
         adapter.setComments(CommentList, user, currentPost, postList);
 
         Button btnGoBackToPid = findViewById(R.id.btnGoBackToPid);
+        Button btnAddComment = findViewById(R.id.addCommentButton);
+
         btnGoBackToPid.setOnClickListener(v ->{
             Intent i = new Intent(this, Pid.class);
             int postIndex = postList.indexOf(currentPost);
             postList.get(postIndex).setComments(CommentList);
             DataHolder.getInstance().setPostList(postList);
+            i.putExtra("USER", user);
+            startActivity(i);
+        });
+        btnAddComment.setOnClickListener(v->{
+            EditText e = findViewById(R.id.commentTextView);
+            String s = e.getText().toString();
+            if(s.length()==0){
+                Toast.makeText(CommentPage.this,
+                        "You can't add a blank comment!",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Comment newC= new Comment(CommentList.size()+1,user.getDisplayName(),s,user.getPhoto());
+            CommentList.add(newC);
+            postList.get(postList.indexOf(currentPost)).setComments(CommentList);
+            postList.get(postList.indexOf(currentPost)).setCommentsNumber(postList.get(postList.indexOf(currentPost)).getCommentsNumber()+1);
+            Intent i = new Intent(this,CommentPage.class);
+            DataHolder.getInstance().setComments(CommentList);
+            DataHolder.getInstance().setPostList(postList);
+            DataHolder.getInstance().setCurrentPost(currentPost);
             i.putExtra("USER", user);
             startActivity(i);
         });
